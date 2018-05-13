e3e048ed98a0c6d75c6ceef21e535f6727cbfd7e
Valentine Gutkin <admin@networklabs.info>
2018-05-13 15:35:55 +0300

    optimization round 1
    combined merge to download adding the new data to file directly instead of saving to temp first
    changed method of getting desired file to argument instead of "hard coding" in script

--- DownloadClass.py
+++ DownloadClass.py
@@ -1,27 +1,19 @@
-import requests, os
+import requests, os, sys
 
 def get_Movie():
     status_code = 200
+    filename = "Lesson.ts"
     counter = 0
-    
+
     while status_code == 200:
-        response = requests.get('https://sslstream.bynetcdn.com/vod/mp4:vod/openu/PRV1/R46zT1CPEZ/App/R46zT1CPEZ_9.mp4/media_b400000_%d.ts' % counter)
+        response = requests.get('%s_%d.ts' % (sys.argv[1], counter))
         status_code = response.status_code
         if status_code == 200:
-            filename = "media_b400000_%d.ts" % counter
-            movie = open(filename, "w")
+            movie = open(filename, "ab")
             movie.write(response.content)
             movie.close()
             counter = counter + 1
     return counter
 
-def merge_Movies(number_of_parts):
-    counter = 0
-    cwd = os.getcwd()
-    while counter < number_of_parts:
-        os.system("cat media_b400000_%d.ts >> Lesson.ts" %counter)
-        os.remove("%s/media_b400000_%d.ts" %(cwd, counter))
-        counter = counter + 1
-
 number_of_parts = get_Movie()
-merge_Movies(number_of_parts)
+
